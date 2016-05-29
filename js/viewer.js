7d60847bbebe3f45a7063522e46a91866a35e7e6
Cameron Sun <cameron.csun@gmail.com>
2016-05-29 01:04:12 -0700

    Added a simple loading bar.

--- js/viewer.js
+++ js/viewer.js
@@ -1,10 +1,11 @@
-function startViewer(containerId, storyFile) {
-  var containerElement = document.getElementById(containerId);
+function startViewer(storyFile) {
+  var containerElement = document.getElementById('viewer-container');
+  containerElement.style.visibility = 'hidden';
 
   var windowWidth = window.innerWidth
     || document.documentElement.clientWidth
     || document.body.clientWidth;
-  var pxRatio = window.devicePixelRatio = window.devicePixelRatio || 
+  var pxRatio = window.devicePixelRatio = window.devicePixelRatio ||
                 window.screen.deviceXDPI / window.screen.logicalXDPI;
 
   var multiplier = Math.round((windowWidth * 0.88) / 16);
@@ -18,10 +19,24 @@ function startViewer(containerId, storyFile) {
   var width = multiplier * pxRatio * 16;
   var height = multiplier * pxRatio * 9;
 
+  var loadingBar = document.getElementById('loading-bar');
+  function loadingCallback(percentage){
+    if(percentage == 1) {
+      var loadingBarContainer = document.getElementById(
+          'loading-bar-container');
+      containerElement.style.visibility = 'visible';
+      loadingBarContainer.style.visibility = 'hidden';
+    }
+    else {
+      loadingBar.style.width = (percentage * 100) + '%';
+    }
+  }
+
   scarry.init({
     size: {width: width, height: height},
     containerElement: containerElement,
-    
+    loadingCallback: loadingCallback,
+
     storyFile: storyFile
   });
 }
